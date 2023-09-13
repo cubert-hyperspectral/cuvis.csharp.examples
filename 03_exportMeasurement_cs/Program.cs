ab97c154c1894e1a1fea1ef95883c99e0124e9e1
Simon Birkholz <birkholz@cubert-gmbh.de>
2023-09-13 16:16:05 +0200

    updated csharp examples to 3.2.1

--- 03_exportMeasurement_cs/Program.cs
+++ 03_exportMeasurement_cs/Program.cs
@@ -13,17 +13,23 @@ namespace ConsoleApp1
                 throw new ArgumentException("Invalid number of cli arguments");
             }
 
-            cuvis_net.General.Init(args[0]);
+            var general_settings = cuvis_net.GeneralExportSettings.Default;
+            general_settings.PanSharpeningAlgorithmType = cuvis_net.PanSharpeningAlgorithm.Noop;
+            general_settings.BlendOpacity = 1.0;
 
+            var general_settings_envi = general_settings;
+            general_settings_envi.ExportDir = "/envi";
 
-            var general_setting_envi = new cuvis_net.GeneralExportSettings(args[3] +"/envi", "all", 1.0, 0.0, cuvis_net.PanSharpeningInterpolationType.NearestNeighbour, cuvis_net.PanSharpeningAlgorithm.Noop, false, false, false, 1);
+            var general_settings_single = general_settings;
+            general_settings_single.ExportDir = "/single";
 
-            var general_setting_single = new cuvis_net.GeneralExportSettings(args[3] + "/single", "all", 1.0, 0.0, cuvis_net.PanSharpeningInterpolationType.NearestNeighbour, cuvis_net.PanSharpeningAlgorithm.Noop, false, false, false, 1);
+            var general_settings_multi = general_settings;
+            general_settings_multi.ExportDir = "/multi";
 
-            var general_setting_multi = new cuvis_net.GeneralExportSettings(args[3] + "/multi", "all", 1.0, 0.0, cuvis_net.PanSharpeningInterpolationType.NearestNeighbour, cuvis_net.PanSharpeningAlgorithm.Noop, false, false, false, 1);
+            var general_settings_view = general_settings;
+            general_settings_view.ExportDir = "/view";
 
-            var general_setting_view = new cuvis_net.GeneralExportSettings(args[3] + "/view", "all", 1.0, 0.0, cuvis_net.PanSharpeningInterpolationType.NearestNeighbour, cuvis_net.PanSharpeningAlgorithm.Noop, false, false, false, 1);
-
+           
             Console.WriteLine("Example 03 export measurement");
 
             Console.WriteLine("loading user settings...");
@@ -34,23 +40,23 @@ namespace ConsoleApp1
 
             Console.WriteLine("loading measurement...");
             cuvis_net.Measurement mesu = sess.GetMeasurement(0);
-            //Debug.Assert(mesu.GetHashCode() != null, " No data found");
+            Debug.Assert(mesu.GetHashCode() != null, " No data found");
 
-            //Debug.Assert((mesu.ProcessingMode != cuvis_net.ProcessingMode.Preview));
+            Debug.Assert((mesu.ProcessingMode != cuvis_net.ProcessingMode.Preview));
 
 
             Console.WriteLine("Export to Envi");
-            var enviExporter = new cuvis_net.EnviExporter(general_setting_envi);
+            var enviExporter = new cuvis_net.EnviExporter(general_settings_envi);
             enviExporter.Apply(mesu);
 
             Console.WriteLine("Export to Multi-Channel Tiff");
             var multi_tiff_settings = new cuvis_net.TiffExportSettings(cuvis_net.TiffCompressionMode.None, cuvis_net.TiffFormat.MultiChannel);
-            var multiTiffExporter = new cuvis_net.TiffExporter(general_setting_multi, multi_tiff_settings);
+            var multiTiffExporter = new cuvis_net.TiffExporter(general_settings_multi, multi_tiff_settings);
             multiTiffExporter.Apply(mesu);
 
             Console.WriteLine("Export to separate Tiffs");
             var single_tiff_settings = new cuvis_net.TiffExportSettings(cuvis_net.TiffCompressionMode.None, cuvis_net.TiffFormat.Single);
-            var singleTiffExporter = new cuvis_net.TiffExporter(general_setting_single, single_tiff_settings);
+            var singleTiffExporter = new cuvis_net.TiffExporter(general_settings_single, single_tiff_settings);
             singleTiffExporter.Apply(mesu);
 
             Console.WriteLine("Export View to file");
@@ -68,7 +74,7 @@ namespace ConsoleApp1
             }
 
             var view_export_settings = new cuvis_net.ViewExportSettings(userpluginCai);
-            var viewExporter = new cuvis_net.ViewExporter(general_setting_view, view_export_settings);
+            var viewExporter = new cuvis_net.ViewExporter(general_settings_view, view_export_settings);
             viewExporter.Apply(mesu);
             Console.WriteLine("finished.");
 
