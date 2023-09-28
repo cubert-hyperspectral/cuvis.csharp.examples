d088ca31f1f75ffe6beb1e4546a08c55ab1e018c
Samuel Canadi <canadi@cubert-gmbh.de>
2023-09-28 11:59:56 +0200

    new example set

--- 03_exportMeasurement_cs/run_Example_03_exportMeasurement_csharp.sh
+++ 03_exportMeasurement_cs/run_Example_03_exportMeasurement_csharp.sh
@@ -3,7 +3,7 @@
 # Usage: 
 # example app path 				[string] E.g. ./03_exportMeasuNET.exe   for windows  
 # user settings directory 		[string] Default is ../../../sample_data/set_examples/settings
-# measurement file (.cu3)		[string] Default is ../../../sample_data/set_examples/set0_lab/x20p_calib_color.cu3s
+# measurement file (.cu3s)		[string] Default is ../../../sample_data/set_examples/set0_single/single.cu3s
 # user plugin file (.xml) 		[string] Default is ../../../sample_data/set_examples/userplugin/cai.xml
 # name of export directory 		[string] The name of your output folder realtive to the example app path
 
@@ -15,7 +15,7 @@ else
     ##########WINDOWS##########
 	../bin/03_exportMeasuNET.exe 															\
 	"../../../sample_data/set_examples/settings"						\
-	"../../../sample_data/set_examples/set0_lab/x20p_calib_color.cu3s" 		\
+	"../../../sample_data/set_examples/set0_single/single.cu3s" 		\
 	"../../../sample_data/set_examples/userplugin/cai.xml"				\
 	"export"																					
 fi
--- 07_recordVideoFromSessionFile_cs/run_Example_07_recordVideoFromSessionfile_csharp.sh
+++ 07_recordVideoFromSessionFile_cs/run_Example_07_recordVideoFromSessionfile_csharp.sh
@@ -2,8 +2,8 @@
 
 # Usage: 
 # example app path 				[string] E.g. ./06_recordVideoNET.exe for windows	
-# user settings directory 		[string] Default is C:/Program Files/cuvis/user/settings for windows or /etc/cuvis/user/settings for linux
-# session file			        [string] Default is C:/Program Files/cuvis/sdk/sample_data/example_session/Ulm_001.cu3s for windows
+# user settings directory 		[string] Default is ../../../sample_data/set_examples/user/settings
+# session file			        [string] Default is ../../../sample_data/set_examples/set1_video/video.cu3s
 # name of recording	directory	[string] The name of your output folder realtive to the example app path
 # exposure time 				[int   ] exposure (integration) time in ms 
 # auto exposure					[bool  ] 1 is "on", 0 is "off"; if "on" the aquisition will start with the given exposure time and then adjust on the fly 
@@ -18,7 +18,7 @@ else
     ##########WINDOWS##########
 	../bin/07_recordVideoFromSessionfileNET.exe 									\
 	"C:/Program Files/cuvis/user/settings"							\
-	"C:/Program Files/cuvis/sdk/sample_data/set0_lab/x20p_calib_color.cu3s"	\
+	"C:/Program Files/cuvis/sdk/sample_data/set1_video/video.cu3s"	\
 	"video01"																\
 	100																		\
 	0																		\
