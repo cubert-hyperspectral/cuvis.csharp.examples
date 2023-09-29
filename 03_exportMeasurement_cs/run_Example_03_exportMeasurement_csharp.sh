2b44023a04a6983fed93fc64d7ce7f633e17d5b9
Simon Birkholz <simon.birkholz@cubert-gmbh.de>
2023-09-29 14:33:24 +0200

    updated path of userplugin to new examples data

--- 03_exportMeasurement_cs/run_Example_03_exportMeasurement_csharp.sh
+++ 03_exportMeasurement_cs/run_Example_03_exportMeasurement_csharp.sh
@@ -4,7 +4,7 @@
 # example app path 				[string] E.g. ./03_exportMeasuNET.exe   for windows  
 # user settings directory 		[string] Default is ../../../sample_data/set_examples/settings
 # measurement file (.cu3s)		[string] Default is ../../../sample_data/set_examples/set0_single/single.cu3s
-# user plugin file (.xml) 		[string] Default is ../../../sample_data/set_examples/userplugin/cai.xml
+# user plugin file (.xml) 		[string] Default is ../../../sample_data/set_examples/plugin/ref/cai.xml
 # name of export directory 		[string] The name of your output folder realtive to the example app path
 
 
@@ -16,7 +16,7 @@ else
 	../bin/03_exportMeasuNET.exe 															\
 	"../../../sample_data/set_examples/settings"						\
 	"../../../sample_data/set_examples/set0_single/single.cu3s" 		\
-	"../../../sample_data/set_examples/userplugin/cai.xml"				\
+	"../../../sample_data/set_examples/plugin/ref/cai.xml"				\
 	"export"																					
 fi
 
